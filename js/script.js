240088b3bfe0e556b387c48b6f6df9af9b64e4a5
Xnanga <Jpeutherer1@hotmail.co.uk>
2021-01-21 22:47:43 +0000

    Created property slider, requires dynamic sizing tweaks.

--- js/script.js
+++ js/script.js
@@ -14,12 +14,21 @@ const homepageHeader = document.querySelector(".homepage-header");
 const allSections = document.querySelectorAll("section");
 const sectionOne = document.querySelector(".section--1");
 
+const propertySliderLeftBtn = document.querySelector(
+  ".property-slider__btn--left"
+);
+const propertySliderRightBtn = document.querySelector(
+  ".property-slider__btn--right"
+);
+
 // Init
 
 const init = function () {
   // Functions on Page Load
   homepageSlider();
+  propertySlider();
   manageTabs("1");
+  hideSections();
 
   // Set Elements on Page Load
   tabs[0].classList.add("search__option-tab--active");
@@ -46,19 +55,29 @@ const init = function () {
 
 // Section Reveal on Scroll
 
+const hideSections = function () {
+  if (window.scrollY > 40) {
+    return;
+  } else {
+    allSections.forEach((section) =>
+      section.classList.add("transparent", "move-down")
+    );
+  }
+};
+
 const revealSection = function (entries) {
   const [entry] = entries;
 
   if (entry.isIntersecting) {
     const intersectingSection = entry.target;
-    intersectingSection.style.opacity = "1.0";
+    intersectingSection.classList.remove("transparent", "move-down");
     sectionObserver.unobserve(intersectingSection);
   }
 };
 
 const sectionObserver = new IntersectionObserver(revealSection, {
   root: null,
-  threshold: 0.5,
+  threshold: 0.25,
 });
 
 allSections.forEach((section) => sectionObserver.observe(section));
@@ -89,6 +108,49 @@ const homepageSlider = function () {
   };
 
   const slideTimer = setInterval(nextSlide, 6000);
+};
+
+// Homepage Property Card Slider
+
+const propertySlider = function () {
+  const propertyCardSlides = document.querySelectorAll(".property-card");
+
+  let currentSlide = 0;
+  const maxSlide = propertyCardSlides.length;
+  const minSlide = 0;
+
+  const goToSlide = function (slide, direction) {
+    if (direction === "next") {
+      propertyCardSlides.forEach(function (s, i) {
+        s.style.transform = `translateX(-${100 * slide}%)`;
+      });
+    } else if (direction === "prev") {
+      propertyCardSlides.forEach(function (s, i) {
+        s.style.transform = `translateX(-${100 * slide}%)`;
+      });
+    }
+  };
+
+  const nextSlide = function () {
+    if (currentSlide === maxSlide - 5) {
+      return;
+    } else {
+      currentSlide++;
+    }
+    goToSlide(currentSlide, "next");
+  };
+
+  const prevSlide = function () {
+    if (currentSlide === minSlide) {
+      return;
+    } else {
+      currentSlide--;
+    }
+    goToSlide(currentSlide, "prev");
+  };
+
+  propertySliderRightBtn.addEventListener("click", nextSlide);
+  propertySliderLeftBtn.addEventListener("click", prevSlide);
 };
 
 // Homepage Header Tabs
